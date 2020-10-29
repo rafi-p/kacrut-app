5e2de654f0e001c65bf88b427eee2f8db6895ac8
torianyap <torianyap@gmail.com>
2020-10-29 10:38:51 +0700

    commited

--- server/app.js
+++ server/app.js
@@ -1,2 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use()
+
+app.listen(port, () => console.log('listening at ' + port))
