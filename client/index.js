b59e613a2ec551ad6ba01535e7726a482438f466
Abraham Purnomo <abraham-sys@github.com>
2020-10-29 18:48:37 +0700

    Added index.html(already SPA dedicated style), including index.js

--- client/index.js
+++ client/index.js
@@ -1,15 +1,16 @@
-const user = "http://localhost:3000/users";
-const todo = "http://localhost:3000/todos";
+const route = "http://localhost:3000";
 
 $(document).ready(() => {
   const token = localStorage.getItem("token");
   if (token) {
-    $("#content").show();
-    $("#landing").hide();
-    fetchTodos();
+    $("#home").show();
+    $("#login").hide();
+    $("#register").hide();
+    // fetchJokes();
   } else {
-    $("#content").hide();
-    $("#landing").show();
+    $("#home").hide();
+    $("#login").show();
+    $("#register").hide();
   }
 
   $("#logout").on("click", () => {
@@ -19,26 +20,65 @@ $(document).ready(() => {
 
 function login(e) {
   e.preventDefault();
-  const email = $("#email").val();
-  const password = $("#password").val();
+  const email = $("#logEmail").val();
+  const password = $("#logPassword").val();
 
   console.log(email, password);
   $.ajax({
     method: "POST",
-    url: user + "/login",
+    url: route + "/login",
     data: {
       email,
       password,
     },
   })
     .done((response) => {
-      const token = response.accessToken;
+      const token = response.access_token;
       localStorage.setItem("token", token);
-      $("#content").show();
-      $("#landing").hide();
-      $("#email").val("");
-      $("#password").val("");
-      fetchTodos();
+      $("#home").show();
+      $("#login").hide();
+      $("#logEmail").val("");
+      $("#logPassword").val("");
+      // fetchTodos();
+    })
+    .fail((err) => {
+      console.log(err);
+    });
+}
+
+function toRegister(e) {
+  e.preventDefault();
+  $("#login").hide();
+  $("#home").hide();
+  $("#register").show();
+}
+
+function toLogin(e) {
+  e.preventDefault();
+  $("#login").show();
+  $("#home").hide();
+  $("#register").hide();
+}
+
+function register(e) {
+  e.preventDefault();
+  const email = $("#registerEmail").val();
+  const password = $("#registerPassword").val();
+
+  console.log(email, password);
+  $.ajax({
+    method: "POST",
+    url: route + "/register",
+    data: {
+      email,
+      password,
+    },
+  })
+    .done((response) => {
+      $("#login").show();
+      $("#register").hide();
+      $("#home").hide();
+      // fetchTodos();
     })
     .fail((err) => {
       console.log(err);
@@ -46,42 +86,43 @@ function login(e) {
 }
 
 function logout() {
-  $("#landing").show();
-  $("#content").hide();
+  $("#login").show();
+  $("#home").hide();
+  $("#register").hide();
   localStorage.removeItem("token");
 }
 
-function fetchTodos() {
-  const token = localStorage.getItem("token");
-  $.ajax({
-    method: "GET",
-    url: todo,
-    headers: {
-      token: token,
-    },
-  })
-    .done((response) => {
-      $("#allTodo").empty();
-      const todos = response;
-      todos.forEach((eachTodo, index) => {
-        const d = new Date(eachTodo.due_date);
-        const date = d.getDate();
-        const month = d.getMonth() + 1;
-        const year = d.getFullYear();
-        const fulldate = `${date}/${month}/${year}`;
-        $("#allTodo").append(
-          ` <tr>
-             <th scope="row">${index + 1}</th>
-             <td>${eachTodo.title}</td>
-             <td>${eachTodo.description}</td>
-             <td>${eachTodo.status}</td>
-             <td>${fulldate}</td>
-             <td><button type="button" class="btn btn-outline-dark">Delete</button> - <button type="button" class="btn btn-outline-dark">Edit</button>
-           </tr>`
-        );
-      });
-    })
-    .fail((err) => {
-      console.log(err);
-    });
-}
+// function fetchJokes() {
+//   const token = localStorage.getItem("token");
+//   $.ajax({
+//     method: "GET",
+//     url: todo,
+//     headers: {
+//       token: token,
+//     },
+//   })
+//     .done((response) => {
+//       $("#allTodo").empty();
+//       const todos = response;
+//       todos.forEach((eachTodo, index) => {
+//         const d = new Date(eachTodo.due_date);
+//         const date = d.getDate();
+//         const month = d.getMonth() + 1;
+//         const year = d.getFullYear();
+//         const fulldate = `${date}/${month}/${year}`;
+//         $("#allTodo").append(
+//           ` <tr>
+//              <th scope="row">${index + 1}</th>
+//              <td>${eachTodo.title}</td>
+//              <td>${eachTodo.description}</td>
+//              <td>${eachTodo.status}</td>
+//              <td>${fulldate}</td>
+//              <td><button type="button" class="btn btn-outline-dark">Delete</button> - <button type="button" class="btn btn-outline-dark">Edit</button>
+//            </tr>`
+//         );
+//       });
+//     })
+//     .fail((err) => {
+//       console.log(err);
+//     });
+// }
